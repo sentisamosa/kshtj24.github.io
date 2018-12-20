bb99fa61202019ff146ee721ff84931dac809034
Acc Team <kshtjtcs@outlook.com>
2018-12-20 18:13:42 +0530

    resume page added

---
+++ pageScript.js
@@ -0,0 +1,16 @@
+$(function () {
+    var specialElementHandlers = {
+        '#editor': function (element,renderer) {
+            return true;
+        }
+    };
+ $('#cmd').click(function () {
+        var doc = new jsPDF();
+        doc.fromHTML(
+            $('#target').html(), 15, 15, 
+            { 'width': 170, 'elementHandlers': specialElementHandlers }, 
+            function(){ doc.save('sample-file.pdf'); }
+        );
+
+    });  
+});

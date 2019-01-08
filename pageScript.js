332db8d3f3c922e4169ac716e2283e2dfef6d4e9
Acc Team <kshtjtcs@outlook.com>
2019-01-08 18:04:23 +0530

    my resume social anchor links added, responsive feature edited in contact form

--- pageScript.js
+++ pageScript.js
@@ -1,21 +1,3 @@
-$(function () {
-    var specialElementHandlers = {
-        '#editor': function (element,renderer) {
-            return true;
-        }
-    };
- $('#cmd').click(function () {
-        var doc = new jsPDF();
-        doc.fromHTML(
-            $('#target').html(), 15, 15, 
-            { 'width': 170, 'elementHandlers': specialElementHandlers }, 
-            function(){ doc.save('sample-file.pdf'); }
-        );
-
-    });  
-});
-
-
 function myFunction() {
   var x = document.getElementById("navDemo");
   if (x.className.indexOf("w3-show") == -1) {
@@ -24,15 +6,3 @@ function myFunction() {
     x.className = x.className.replace(" w3-show", "");
   }
 }
-
-
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDYAi-g6LUhoBemBkJbC00paYaBI07D2YA",
-    authDomain: "mywebsite-65acf.firebaseapp.com",
-    databaseURL: "https://mywebsite-65acf.firebaseio.com",
-    projectId: "mywebsite-65acf",
-    storageBucket: "mywebsite-65acf.appspot.com",
-    messagingSenderId: "799508371201"
-  };
-  firebase.initializeApp(config);

2ef548f1b7ffe7d48f74eb7787e7953ecfc04341
Acc Team <kshtjtcs@outlook.com>
2019-01-09 18:13:10 +0530

    changes started in index page

--- scripts/pageScript.js
+++ scripts/pageScript.js
@@ -6,3 +6,30 @@ function myFunction() {
     x.className = x.className.replace(" w3-show", "");
   }
 }
+
+function indexOnLoad(){
+	var headingText = 'I am Kshitij Saxena';
+	const elemnt = document.getElementById('heading');
+	
+	recursiveTyper(headingText, elemnt);
+}
+
+
+function recursiveTyper(txt, elemnt){
+	
+	if(txt.length > 0){
+		elemnt.innerHTML += txt.charAt(0);
+		txt = txt.substring(1,txt.length);
+		setTimeout(recursiveTyper(txt,elemnt),5000);
+	}	
+}
+
+// function recursiveTyper(txt, elemnt){
+	// var i = 0;
+	
+	// if(i < txt.length){
+		// elemnt.innerHTML += txt.charAt(i);
+		// i++;
+		// setTimeout(recursiveTyper(txt,elemnt),50);
+	// }	
+// }

f9e4d421159089faeed3597e5ce656eb34680bf9
Kshitij Saxena <kshtj246@outlook.com>
2020-08-10 23:20:34 +0530

    app file updated and profile pic added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
+import profilepic from "./assets/profilepic.jpg";
+import { Switch, Route, Link } from "react-router-dom";
 
+const pages = [{}, {}, {}, {}];
 class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <Switch>
+        <Route path="/">
+          <div>
+            <img
+              src={profilepic}
+              alt="profilePic"
+              className="rounded-circle mx-auto d-block img-fluid w-25"
+            />
+          </div>
+        </Route>
+      </Switch>
+    );
   }
 }
 

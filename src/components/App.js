6cd59074cfeda50b009bc122bd0fdfa2a43efb98
Kshitij Saxena <kshtj246@outlook.com>
2020-08-11 00:57:00 +0530

    theme changed

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,15 @@ const pages = [{ pageName: "Home", pagePath: "/", pageItem: <Home /> }, {}, {}];
 class App extends Component {
   render() {
     return (
-      <Switch>
-        {pages.map((item, key) => (
-          <Route path={item.pagePath} key={key}>
-            <div className="container">{item.pageItem}</div>
-          </Route>
-        ))}
-      </Switch>
+      <div>
+        <Switch>
+          {pages.map((item, key) => (
+            <Route path={item.pagePath} key={key}>
+              <div>{item.pageItem}</div>
+            </Route>
+          ))}
+        </Switch>
+      </div>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,16 +10,18 @@ class Home extends Component {
           alt="MainImage"
           className="img-fluid w-25 rounded-circle d-block mx-auto mt-5"
         />
-        <h2 className="mx-auto w-50 text-center mt-2">Hello, I am Kshitij</h2>
+        <h2 className="mx-auto w-50 text-center mt-2 text-white">
+          Hello, I am Kshitij
+        </h2>
 
         <div className="mx-auto w-50 text-center mt-5">
-          <a href="/#" className="btn btn-outline-dark ml-1 btn-lg">
+          <a href="/#" className="btn btn-outline-light btn-lg">
             My Resume
           </a>
-          <a href="/#" className="btn btn-outline-dark ml-1 btn-lg">
+          <a href="/#" className="btn btn-outline-light ml-2 btn-lg">
             My Work
           </a>
-          <a href="/#" className="btn btn-outline-dark ml-1 btn-lg">
+          <a href="/#" className="btn btn-outline-light ml-2 btn-lg">
             Contact Me
           </a>
         </div>
